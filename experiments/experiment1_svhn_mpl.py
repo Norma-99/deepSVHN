67139c56c90bd58ac7e0772097b5c8024efcf760
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2020-02-09 18:24:25 +0100

    experiments

--- experiments/experiment1_svhn_mpl.py
+++ experiments/experiment1_svhn_mpl.py
@@ -12,6 +12,7 @@ EPOCHS = 100
 
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(1024, activation='relu'),
     tf.keras.layers.Dense(256, activation='relu'),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(10, activation='softmax')
