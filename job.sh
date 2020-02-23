68afd3c2c212a1c71d0b4a7dcef5e64f3c597f7c
musergi <sergi.mus@gmail.com>
2020-02-23 15:43:07 +0100

    Prepared for fase 1

--- job.sh
+++ job.sh
@@ -8,6 +8,6 @@
 
 PYTHON="/scratch/nas/4/norma/venv/bin/python"
 
-$PYTHON prepare_dataset.py
-#$PYTHON experiments/experiment1_svhn_mpl.py
+#$PYTHON prepare_dataset.py
+$PYTHON experiments/experiment1_svhn_mpl.py
 #$PYTHON experiments/experiment2_svhn_cnn.py
