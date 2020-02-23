6ab596f146c97f87f19030d9a54f6c1754973f89
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2020-02-23 19:46:27 +0100

    Created job_mlp.sh

---
+++ job_mlp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --chdir=/scratch/nas/4/norma/deepSVHN
+#SBATCH --job-name="deepSVHN"
+#SBATCH --output=/scratch/nas/4/norma/.log/stdout-%j.out
+#SBATCH --error=/scratch/nas/4/norma/.log/stderr-%j.out
+#SBATCH --wait-all-nodes=1
+
+PYTHON="/scratch/nas/4/norma/venv/bin/python"
+
+$PYTHON prepare_dataset.py
+$PYTHON experiments/experiment2_svhn_cnn.py
